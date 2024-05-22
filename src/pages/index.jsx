83448aa235cedeb3e7d75d3eedc358c4a250cbb7
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-05-22 18:21:58 +0200

    02

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,10 @@ import './index.css';
 import { Header } from '../components/Header/header';
 import { Intro } from '../components/Intro/intro';
 import { Pose } from '../components/Pose/pose';
+import yoga from '../img/yoga-pose.jpg'
  
+//       <Pose src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/cviceni-jogini/master/img/yoga-pose.jpg" />
 
-// <Pose src="../img/yoga-pose.jpg" />
 
 const HomePage = () => {
   return( 
@@ -16,7 +17,7 @@ const HomePage = () => {
       Postupně se seznámíte se základními principy jógy, jak přístupovat k
       sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu
       svoje tělo, pocity a emoce."/>
-      <Pose src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/cviceni-jogini/master/img/yoga-pose.jpg" />
+      <Pose src={yoga} />
     </div>
   )
 }
